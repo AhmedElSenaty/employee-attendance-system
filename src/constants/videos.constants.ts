cf7eb7b5b96ed0839ed1478cbd5ff39eb4dbd7cd
Mohamed-0-Turki <mohamedtarek234234@gmail.com>
2025-06-16 05:37:41 +0300

    update videos

--- src/constants/videos.constants.ts
+++ src/constants/videos.constants.ts
@@ -1,13 +1,35 @@
-export const LOGS_VIDEO = "https://www.youtube.com/embed/N94Q0SmpXig?si=8WUI07cry7MjToHn"
-export const CALENDER_VIDEO = "https://www.youtube.com/embed/N94Q0SmpXig?si=8WUI07cry7MjToHn"
-export const SYSTEM_DATA_VIDEO = "https://www.youtube.com/embed/dQw4w9WgXcQ"
-export const DEVICES_VIDEO = "https://www.youtube.com/embed/dQw4w9WgXcQ"
-export const ATTENDANCE_VIDEO = "https://www.youtube.com/embed/dQw4w9WgXcQ"
-export const ATTENDANCE_OVERVIEW_VIDEO = "https://www.youtube.com/embed/dQw4w9WgXcQ"
-export const ATTENDANCE_VACATION_VIDEO = "https://www.youtube.com/embed/dQw4w9WgXcQ"
-export const LEAVE_REQUESTS_MANAGER_VIDEO = "https://www.youtube.com/embed/dQw4w9WgXcQ"
-export const LEAVE_REQUESTS_EMPLOYEE_VIDEO = "https://www.youtube.com/embed/dQw4w9WgXcQ"
-export const MISSION_REQUESTS_MANAGER_VIDEO = "https://www.youtube.com/embed/dQw4w9WgXcQ"
-export const MISSION_REQUESTS_EMPLOYEE_VIDEO = "https://www.youtube.com/embed/dQw4w9WgXcQ"
+export const LOGS_VIDEO =
+  "https://www.youtube.com/embed/N94Q0SmpXig?si=8WUI07cry7MjToHn";
+
+export const CALENDER_VIDEO =
+  "https://www.youtube.com/embed/N75508C2hbs?si=D34e6srFNk2G3vZf";
+
+export const SYSTEM_DATA_VIDEO = "https://www.youtube.com/embed/dQw4w9WgXcQ";
+
+export const DEVICES_VIDEO =
+  "https://www.youtube.com/embed/-35TdV6ro-w?si=ZA4hdd9ukHSVG9Um";
+
+export const ATTENDANCE_VIDEO =
+  "https://www.youtube.com/embed/1OtHVXCYRKE?si=gFbOgUfnOntLXmUk";
+
+export const ATTENDANCE_OVERVIEW_VIDEO =
+  "https://www.youtube.com/embed/rlEfsEVSkts?si=S3dtrqTCq3eQ_G_q";
+
+export const ATTENDANCE_VACATION_VIDEO =
+  "https://www.youtube.com/embed/LX8n7SDgnkY?si=r5xjdn-Kv4QOhvpl";
+
+export const LEAVE_REQUESTS_MANAGER_VIDEO =
+  "https://www.youtube.com/embed/Z5yx_szNx7Y?si=mbyUFV6AYf01Z_rw";
+
+export const LEAVE_REQUESTS_EMPLOYEE_VIDEO =
+  "https://www.youtube.com/embed/JxAqaMFEVBY?si=td9ZDtjejShtuo7s";
+
+export const MISSION_REQUESTS_MANAGER_VIDEO =
+  "https://www.youtube.com/embed/dQw4w9WgXcQ";
+export const MISSION_REQUESTS_EMPLOYEE_VIDEO =
+  "https://www.youtube.com/embed/dQw4w9WgXcQ";
+
+// export const MISSION_REQUESTS_EMPLOYEE_VIDEO =
+// "https://www.youtube.com/embed/dQw4w9WgXcQ";
 export const ORDINARY_REQUESTS_MANAGER_VIDEO = "https://www.youtube.com/embed/dQw4w9WgXcQ"
 export const ORDINARY_REQUESTS_EMPLOYEE_VIDEO = "https://www.youtube.com/embed/dQw4w9WgXcQ"
