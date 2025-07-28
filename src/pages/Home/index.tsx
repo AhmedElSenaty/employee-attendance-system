cc59b6dafe1bb48040d15b3d9ef837043f25f03d
AhmedElSenaty <ahmedmohamedomarelsenaty@gmail.com>
2025-07-28 15:48:07 +0300

    add: goto home screen in apk

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,15 @@
 import { HeroSection, SystemFeaturesSection } from "./views"
-
+import { Capacitor } from '@capacitor/core';
+import { useNavigate } from "react-router";
 export const HomePage = () => {
+
+    const navigate = useNavigate();
+
+    if (Capacitor.getPlatform() === 'android') {
+        navigate("/login");
+        return;
+    }
+    
   return (
     <>
       <HeroSection />
