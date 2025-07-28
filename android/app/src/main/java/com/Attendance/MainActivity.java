f3c48c56b77595861463133fe1d578ced7a9249f
AhmedElSenaty <ahmedmohamedomarelsenaty@gmail.com>
2025-07-28 15:44:12 +0300

    add: icon

--- android/app/src/main/java/com/Attendance/MainActivity.java
+++ android/app/src/main/java/com/Attendance/MainActivity.java
@@ -1,5 +1,18 @@
 package com.Attendance;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+
+
+public class MainActivity extends BridgeActivity {
+    super.onCreate(savedInstanceState);
+
+    // Allow HTTPS page (https://localhost) to load HTTP API (http://192.168.1.7:3000)
+    this.bridge.getWebView()
+      .getSettings()
+      .setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+  
+}
