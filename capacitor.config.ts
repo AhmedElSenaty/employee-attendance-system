e6cc7660d527d589d9f72c3677e55024bbed00eb
AhmedElSenaty <ahmedmohamedomarelsenaty@gmail.com>
2025-07-28 15:05:28 +0300

    fix: init cap

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.Attendance',
+  appName: 'Attendance',
+  webDir: 'dist'
+};
+
+export default config;
