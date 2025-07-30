c653f0b5b829f10646fbd4ca812157b9af64ac3b
AhmedElSenaty <ahmedmohamedomarelsenaty@gmail.com>
2025-07-30 17:36:10 +0300

    fixd: redirection

--- android/app/src/main/java/com/Attendance/MainActivity.java
+++ android/app/src/main/java/com/Attendance/MainActivity.java
@@ -6,17 +6,15 @@ import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
 
-
 public class MainActivity extends BridgeActivity {
-    
-    protected void onCreate(Bundle savedInstanceState) {
 
-        super.onCreate(savedInstanceState);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
 
-        // Allow HTTPS page (https://localhost) to load HTTP API (http://192.168.1.7:3000)
-        this.bridge.getWebView()
-        .getSettings()
-        .setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
-    }
-  
+    // Allow HTTPS page (https://localhost) to load HTTP API (http://192.168.1.7:3000)
+    this.bridge.getWebView()
+      .getSettings()
+      .setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
+  }
 }
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,13 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.Attendance',
-  appName: 'Attendance',
-  webDir: 'dist'
+    appId: 'com.Attendance',
+    appName: 'Attendance',
+    webDir: 'dist',
+    server: {
+        url: 'http://193.227.34.53:5173',
+        cleartext: true
+    }
 };
 
 export default config;
